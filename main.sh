0f83ff43c9067b6022f9a19949ebd4c1f66c1ae5
kambaikariuki <kambaikariuki@gmail.com>
2024-06-10 02:39:51 -0700

    Inserted Menu into main.sh

---
+++ main.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Menu
+printf "Welcome to the ALU registration system\n
+        Menu
+ 1. Add new student
+ 2. View all students
+ 3. Delete student record
+ 4. Update student record
+ 5. Sort student email 
+ 6. Exit 
+ 
+ Enter your choice\n"
+
