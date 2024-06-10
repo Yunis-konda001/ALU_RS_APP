c1f2dd279b569685ce1ca0b664be840752aebf1a
Blandin3 <b.iradukund3@alustudent.com>
2024-06-10 12:49:17 -0700

    completed back up server script

--- backup-Negpod_2.sh
+++ backup-Negpod_2.sh
@@ -5,3 +5,9 @@ HOST="ec1ebc754f75.a5efec7a.alu-cod.online"
 USERNAME="ec1ebc754f75"
 PASSWORD="ea1d8f222b11d7be18f6"
 BACKUP_DIR="/summative/0524-2024m"
+
+# Directory to backup
+LOCAL_DIR="negpod_id-q1"
+
+# Create backup using rsync
+rsync -avz -e "sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no" $LOCAL_DIR $USERNAME@$HOST:$BACKUP_DIR
